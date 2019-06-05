43ff7959df173e225f323a4ef94e927be07897a8
liyongjie <liyongjie@tvflnet.com>
2019-06-05 13:45:57 +0800

    我的模块列表实现，添加全局样式

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -1,14 +1,18 @@
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, TouchableOpacity, ScrollView } from 'react-native';
 import { connect } from 'react-redux'
 import actions from '../action/index'
 import NavigationUtil from '../navigator/NavigationUtil'
 import NavigationBar from '../common/NavigationBar'
 import Feather from 'react-native-vector-icons/Feather'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import { MORE_MENU } from '../common/MORE_MENUE';
+import GlobalStyles from '../res/GlobalStyles';
+import ViewUtil from '../util/ViewUtil';
 
-const THEME_COLOR = 'orange'
+// const THEME_COLOR = 'orange'
+const THEME_COLOR = '#678'
 
 class MyPage extends Component {
     getRightButton() {
@@ -36,7 +40,7 @@ class MyPage extends Component {
             <View style={{ flexDirection: 'row' }}>
                 <TouchableOpacity
                     style={{ padding: 8, paddingLeft: 12 }}
-                    onPress={()=>{}}
+                    onPress={() => { }}
                 >
                     <Ionicons
                         name='ios-arrow-back'
@@ -47,6 +51,13 @@ class MyPage extends Component {
                 </TouchableOpacity>
             </View>
         )
+    }
+    onClick() {
+
+    }
+    getItem(menu) {
+        const { theme } = this.props;
+        return ViewUtil.getMenuItem(() => this.onClick(menu), menu, THEME_COLOR);
     }
     render() {
         let statusBar = {
@@ -64,47 +75,102 @@ class MyPage extends Component {
         )
 
         return (
-            <View style={styles.container}>
+            <View style={GlobalStyles.root_container}>
                 {navigationBar}
-                <Text style={styles.welcome}>MyPage</Text>
-                <Button
-                    title="改变主题颜色"
-                    onPress={() => {
-                        this.props.onThemeChange('#159')
-                    }}
-                ></Button>
 
-                <Text onPress={() => {
-                    NavigationUtil.goPage(null, "DetailPage")
-                }}>跳转详情页</Text>
-                <Button title="跳转Fetch页面" onPress={() => {
-                    NavigationUtil.goPage(null, "FetchDemoPage")
-                }}></Button>
-                <Button title="离线缓存框架" onPress={() => {
-                    NavigationUtil.goPage(null, "DataStoreDemoPage")
-                }}></Button>
+                <ScrollView>
+                    <TouchableOpacity
+                        style={styles.item}
+                        onClick={this.onClick(MORE_MENU.About)}
+                    >
+                        <View style={styles.about_left}>
+                            <Ionicons
+                                name={MORE_MENU.About.icon}
+                                size={40}
+                                style={{ marginRight: 10, color: THEME_COLOR }}
+                            />
+                            <Text>GitHub Popular</Text>
+                        </View>
+                        <Ionicons
+                            name={'ios-arrow-forward'}
+                            size={16}
+                            style={{
+                                marginRight: 10,
+                                alignSelf: 'center',
+                                color: THEME_COLOR
+                            }}
+                        />
+                    </TouchableOpacity>
+                    <View style={GlobalStyles.line} />
+
+                    {this.getItem(MORE_MENU.Tutorial)}
+                    {/*趋势管理*/}
+                    <Text style={styles.groupTitle}>趋势管理</Text>
+                    {/*自定义语言*/}
+                    {this.getItem(MORE_MENU.Custom_Language)}
+                    {/*语言排序*/}
+                    <View style={GlobalStyles.line} />
+                    {this.getItem(MORE_MENU.Sort_Language)}
+
+                    {/*最热管理*/}
+                    <Text style={styles.groupTitle}>最热管理</Text>
+                    {/*自定义标签*/}
+                    {this.getItem(MORE_MENU.Custom_Key)}
+                    {/*标签排序*/}
+                    <View style={GlobalStyles.line} />
+                    {this.getItem(MORE_MENU.Sort_Key)}
+                    {/*标签移除*/}
+                    <View style={GlobalStyles.line} />
+                    {this.getItem(MORE_MENU.Remove_Key)}
+
+                    {/*设置*/}
+                    <Text style={styles.groupTitle}>设置</Text>
+                    {/*自定义主题*/}
+                    {this.getItem(MORE_MENU.Custom_Theme)}
+                    {/*关于作者*/}
+                    <View style={GlobalStyles.line} />
+                    {this.getItem(MORE_MENU.About_Author)}
+                    <View style={GlobalStyles.line} />
+                    {/*反馈*/}
+                    {this.getItem(MORE_MENU.Feedback)}
+                    <View style={GlobalStyles.line} />
+                    {this.getItem(MORE_MENU.CodePush)}
+                </ScrollView>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
-
-// const mapStateToProps = state => {
-
-// }
 
 const mapDispatchToProps = dispatch => ({
     onThemeChange: theme => dispatch(actions.onThemeChange(theme))
 })
 
 export default connect(null, mapDispatchToProps)(MyPage)
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 30 : 0
+    },
+    about_left: {
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    groupTitle: {
+        marginLeft: 10,
+        marginTop: 10,
+        marginBottom: 5,
+        fontSize: 12,
+        color: 'gray'
+    }
+});
